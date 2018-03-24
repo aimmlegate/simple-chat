99326bfdcf98e023a319387110d49c01338614d4
Andrey Lukin <aimm.legate@gmail.com>
2018-03-24 14:28:00 +0500

    dev

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,38 @@
+'head': {
+    defaults: {
+        title: 'Задание - простой чат',
+        useSocialMetaTags: false
+    }
+},
+
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+},
+
+__pages: [{
+                name: 'includes',
+                href: 'includes.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'messages',
+                href: 'messages.html'
+             },{
+                name: 'no-message',
+                href: 'no-message.html'
+             }]
